26a7ac4bdbf44e19c16b9060dd3855c987f93ffc
Namwoosung <142873609+Namwoosung@users.noreply.github.com>
2025-07-29 22:27:41 +0900

    update additional code

--- 남우성/7차시/1238.java
+++ 남우성/7차시/1238.java
@@ -90,3 +90,86 @@ public class Main {
 		}
 	}
 }
+
+
+/*
+// 더 효과적인 풀이
+// 기존에는 i -> X를 구하기 위해 각 모든 i마다 다익스트라를 구해서 총 N^2 logN 시간 복잡도
+// 아래 코드는 i -> X를 구하기 위해 역방향 그래프를 설정하고, X -> i를 구하는 것으로 한 번에 처리 => NlogN 시간 복잡도
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M, X;
+    static List<List<Node>> graph = new ArrayList<>();
+    static List<List<Node>> reverseGraph = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken()) - 1;
+
+        for (int i = 0; i < N; i++) {
+            graph.add(new ArrayList<>());
+            reverseGraph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken()) - 1;
+            int v = Integer.parseInt(st.nextToken()) - 1;
+            int w = Integer.parseInt(st.nextToken());
+
+            graph.get(u).add(new Node(v, w));         // 정방향
+            reverseGraph.get(v).add(new Node(u, w));  // 역방향
+        }
+
+        int[] toX = dijkstra(reverseGraph, X); // i → X == X에서 역방향 출발
+        int[] fromX = dijkstra(graph, X);      // X → i
+
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            result = Math.max(result, toX[i] + fromX[i]);
+        }
+
+        System.out.println(result);
+    }
+
+    static int[] dijkstra(List<List<Node>> g, int start) {
+        int[] dist = new int[N];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        boolean[] visited = new boolean[N];
+
+        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(n -> n.weight));
+        pq.offer(new Node(start, 0));
+        dist[start] = 0;
+
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
+
+            if (visited[cur.pos]) continue;
+            visited[cur.pos] = true;
+
+            for (Node next : g.get(cur.pos)) {
+                if (!visited[next.pos] && dist[next.pos] > dist[cur.pos] + next.weight) {
+                    dist[next.pos] = dist[cur.pos] + next.weight;
+                    pq.offer(new Node(next.pos, dist[next.pos]));
+                }
+            }
+        }
+        return dist;
+    }
+
+    static class Node {
+        int pos, weight;
+        Node(int pos, int weight) {
+            this.pos = pos;
+            this.weight = weight;
+        }
+    }
+}
+
+*/
