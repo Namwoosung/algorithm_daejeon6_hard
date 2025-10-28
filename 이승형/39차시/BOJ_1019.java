d1d391a9c09e4ddc4ad0bca646475b458d1c824d
cramfull <tmdgud99@gmail.com>
2025-10-29 01:36:10 +0900

    [39차시] 이승형 - 1019번 책 페이지 (failed)

---
+++ 이승형/39차시/BOJ_1019.java
@@ -0,0 +1,15 @@
+package com.ssafy.d1029;
+
+/*
+ * 완탐은 N이 10억이라 무조건 시간초과일 것 같고..
+ * 도저히 풀이가 생각이 안나는데..
+ * 20분 순수 고민하다 풀이 보고 '수학, 구현'이라..
+ * 음.. 조금 더 수련해야겠다..
+ */
+public class BOJ_1019 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
